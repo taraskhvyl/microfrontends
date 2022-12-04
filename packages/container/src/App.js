4f44e6b0192c1ea67b5d7d18d48d577de900a79f
Taras <taras.khvyl@gmail.com>
2022-12-04 11:52:58 +0100

    WIP: added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,20 +1,35 @@
 import { createGenerateClassName, StylesProvider } from "@material-ui/core";
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import { BrowserRouter, Redirect, Route, Router, Switch } from "react-router-dom";
+import router from "react-router-dom/es/Router";
 import Header from "./components/Header";
 import Progress from "./components/Progress";
+import { createBrowserHistory } from "history";
 
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    if(isSignedIn) {
+      history.push('/dashboard');
+    } else {
+      history.push('/');
+    }
+  }, [isSignedIn]);
+
+
   return(
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
       <div>
         <Header onSignOut={() => setIsSignedIn(false)}  isSignedIn={isSignedIn} />
@@ -23,11 +38,15 @@ export default () => {
             <Route path='/auth'>
               <AuthLazy onSignIn={() => setIsSignedIn(true)} />
             </Route>
+            <Route path='/dashboard'>
+              {!isSignedIn && <Redirect to={'/'} />}
+              <DashboardLazy />
+            </Route>
             <Route path='/' component={MarketingLazy}></Route>
           </Switch>
         </Suspense>
       </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
     )
 }
